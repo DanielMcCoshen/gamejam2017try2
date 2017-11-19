1daac31a2627d4d0ec46181331a9f58169df8181
B3amz <omegajpboy@gmail.com>
2017-11-19 02:11:10 -0600

    cheerios

---
+++ Scripts/cheerioSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cheerioSpawner : MonoBehaviour {
+
+    public GameObject cheerio;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("spawnio");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator spawnio()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.03f);
+            Destroy(Instantiate(cheerio, new Vector3(Random.Range(transform.position.x - 5, transform.position.x + 5), transform.position.y, Random.Range(transform.position.z - 5, transform.position.z + 5)), Random.rotation), 10f);
+        }
+    }
+}
