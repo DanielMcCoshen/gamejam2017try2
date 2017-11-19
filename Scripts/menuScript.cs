39c87a2e2868a1ef66bcac26687fd09a9b3943aa
B3amz <omegajpboy@gmail.com>
2017-11-19 05:53:56 -0600

    Menu WIP

---
+++ Scripts/menuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onLevelSelect(string lev)
+    {
+        SceneManager.LoadScene(lev);
+    }
+    public void onLevelRandom(string[] levs)
+    {
+        SceneManager.LoadScene(levs[Random.Range(0, levs.Length)]);
+    }
+
+    public void onExit()
+    {
+        Application.Quit();
+    }
+}
+
